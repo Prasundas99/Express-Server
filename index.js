5bed3fce85f8199592d3c9eb2371dfde1096613d
Prasundas99 <58937669+Prasundas99@users.noreply.github.com>
2021-02-04 18:52:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,16 +14,3 @@ const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-
-
-
-
-//mongooshe schema json schema
-// For Testing mongoose Schema
-import mongoose from "mongoose";
-import jsonSchemas from "mongoose-schema-jsonschema";
-import { userSchema } from "./models/User.js";
-jsonSchemas(mongoose);
-
-const jsonSchema = userSchema.jsonSchema();
-console.dir(jsonSchema, { depth: null });
